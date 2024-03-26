2e124494bcdd41891086a270f07989f16bf8360f
Anna Muller <amuller2@nd.edu>
2024-03-26 16:30:17 -0400

    login and register added

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -2,6 +2,10 @@ import Main from "./Main/Main.js";
 import Form from "./Form/Form.js";
 import Footer from "./Footer/Footer.js";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AuthModule from "./Auth/Auth.js";
+import AuthRegister from "./Auth/AuthRegister";
+import AuthLogin from "./Auth/AuthLogin";
+import ProtectedRoute from "./ProtectedRoute/ProtectedRoute.js";
 
 //This is where the main components are, so far we have main and a user input formthat doesn't connect to anything
 export default function Components() {
@@ -9,6 +13,9 @@ export default function Components() {
     <Router>
       <Footer />
       <Routes>
+        <Route path="/auth" element={<AuthModule />} />
+        <Route path="/auth/register" element={<AuthRegister />} />
+        <Route path="/auth/login" element={<AuthLogin />} />
         <Route path="/" element={<Main />} />
         <Route path="/form" element={<Form />} />
       </Routes>
