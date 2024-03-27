53f994ce6414fd1737cbee3d502e39865229a5d0
Anna Heger <annaheger@aheger01-mac.local>
2024-03-27 15:45:22 -0400

    Protected Routes added

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -1,7 +1,7 @@
 import Main from "./Main/Main.js";
 import Form from "./Form/Form.js";
 import Footer from "./Footer/Footer.js";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import AuthModule from "./Auth/Auth.js";
 import AuthRegister from "./Auth/AuthRegister";
 import AuthLogin from "./Auth/AuthLogin";
@@ -16,8 +16,17 @@ export default function Components() {
         <Route path="/auth" element={<AuthModule />} />
         <Route path="/auth/register" element={<AuthRegister />} />
         <Route path="/auth/login" element={<AuthLogin />} />
-        <Route path="/" element={<Main />} />
-        <Route path="/form" element={<Form />} />
+        <Route
+          path="/"
+          element={<ProtectedRoute path="/" element={Main} />}
+        />
+        <Route
+          path="/form"
+          element={<ProtectedRoute path="/form" element={Form} />}
+        />
+        <Route path="*" element={<Navigate to="/auth" replace />} />
+        {/* <Route path="/" element={<Main />} />
+        <Route path="/form" element={<Form />} /> */}
       </Routes>
     </Router>
   );
