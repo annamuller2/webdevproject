086ef5cc91d21b49529efed9ffd22918b6513384
Anna Muller <amuller2@nd.edu>
2024-03-27 17:14:28 -0400

    protected route working correctly

--- src/Components/ProtectedRoute/ProtectedRoute.js
+++ src/Components/ProtectedRoute/ProtectedRoute.js
@@ -1,21 +1,39 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
 import { checkUser } from "../Auth/AuthService";
 
 // You can pass props using the spread operator to throw them on an object if there are too many to break out
-const ProtectedRoute = ({ element: Component, ...rest }) => {
-  console.log("element: ", Component);
-  const navigate = useNavigate();
-//   const goBackHandler = () => {
-//     navigate("/auth");
-//   };
-  if (checkUser()) {
-    return <Component />;
-  } else {
-    return (
-        navigate("/auth")
-    );
-  }
-};
+// const ProtectedRoute = ({ element: Component, ...rest }) => {
+//   console.log("element: ", Component);
+//   const navigate = useNavigate();
+// //   const goBackHandler = () => {
+// //     navigate("/auth");
+// //   };
+//   if (checkUser()) {
+//     return <Component />;
+//   } else {
+//     return (
+//         navigate("/auth")
+//     );
+//   }
+// };
 
-export default ProtectedRoute;
+
+
+// export default ProtectedRoute;
+
+const ProtectedRoute = ({ element: Component, ...rest }) => {
+
+    if (checkUser()) {
+  
+      return <Component {...rest} />;
+  
+    } else {
+  
+      return <Navigate to="/auth" replace />;
+  
+    }
+  
+  };
+  
+  export default ProtectedRoute;
