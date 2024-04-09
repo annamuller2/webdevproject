a276def7d34cc8bc5d240a59d4ceaa779318eec2
Anna Heger <annaheger@aheger01-mac.dhcp.nd.edu>
2024-04-09 16:47:43 -0400

    Styling the Navbar

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -28,8 +28,8 @@ import AuthLogout from "../Auth/AuthLogout.js"
     //This is the html for the page, which contains a header, and the list of recipes in the JSON, and logout button
     return (
         <div>
-          <h1>Digital Cookbook</h1>
-          Welcome to your personalized digital cookbook!
+          <h1 class="mb-4 mt-6 text-2xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white" >Digital Cookbook</h1>
+          <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400" >Browse recipes from our database!</p>
           <RecipeList recipes={recipes} />
           <AuthLogout/>
         </div>
