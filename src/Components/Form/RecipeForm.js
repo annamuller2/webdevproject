abd704b75038559964e0b20734a633651c1153a7
Anna Heger <annaheger@aheger01-mac.local>
2024-04-16 15:50:32 -0400

    changes

--- src/Components/Form/RecipeForm.js
+++ src/Components/Form/RecipeForm.js
@@ -33,3 +33,82 @@ const RecipeForm = ({ recipes, onSubmitClick }) => {
 };
 
 export default RecipeForm;
+
+
+{/* <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <div>
+      <p>
+          <label> Recipe Name: </label>
+          <input type="text" name="recipeName" />
+        </p>
+      <p>Select the recipe type:</p>
+      <select name="type" id="type">
+        <option value="appetizer">Appetizer</option>
+        <option value="mainDish">Main Dish</option>
+        <option value="sideDish">Side Dish</option>
+        <option value="snack">Snack</option>
+        <option value="dessert">Dessert</option>
+        <option value="other">Other</option>
+      </select>
+      <br/> <br/>
+      <p>Ingredients:</p>
+        {ingredientRows.map((row, index) => (
+          <div key={index}>
+            <label>Amount: </label>
+            <input
+              type="text"
+              name="amount"
+              placeholder="2"
+              value={row.amount}
+              onChange={(e) => handleIngredientInputChange(e, index)}
+            />
+            <label>Unit: </label>
+            <input
+              type="text"
+              name="unit"
+              placeholder="cups"
+              value={row.unit}
+              onChange={(e) => handleIngredientInputChange(e, index)}
+            />
+            <label>Name: </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="brown sugar"
+              value={row.name}
+              onChange={(e) => handleIngredientInputChange(e, index)}
+            />
+            <label>Description: </label>
+            <input
+              type="text"
+              name="detail"
+              placeholder="packed"
+              value={row.detail}
+              onChange={(e) => handleIngredientInputChange(e, index)}
+            />
+          </div>
+        ))}
+        <button onClick={handleAddIngredientRow}>Add Row</button>
+        <br/> <br/>
+
+
+    <p>Directions:</p>
+        {directionRows.map((row, index) => (
+          <div key={index}>
+            <label>Direction: </label>
+            <input
+              type="text"
+              name="direction"
+              placeholder="Combine ingredients in large mixing bowl."
+              value={row.direction}
+              onChange={(e) => handleDirectionInputChange(e, index)}
+            />
+          </div>
+        ))}
+        <button onClick={handleAddDirectionRow}>Add Row</button>
+        <br/> <br/>
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+    </div> */}
